902aae16adcef6c8fe2ac06feac6f829fa4f86b0
Christian Paranas <christiannparanas@gmail.com>
2020-08-21 13:03:39 +0800

    Nav and base styled components

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default function Navbar() {
+   return (
+      <nav>
+         <div>
+            <Link to="/">
+               Learn.Build.<span>Type.</span>
+            </Link>
+         </div>
+         <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/highscores">highscores</Link></li>
+         </ul>
+      </nav>
+   )
+}
