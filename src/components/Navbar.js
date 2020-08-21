3ecab1140109210991b8b064f2a0c0c0e8b605fe
Christian Paranas <christiannparanas@gmail.com>
2020-08-21 13:46:55 +0800

    styled the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  StyledNavbar,
+  StyledNavBrand,
+  StyledNavItems,
+  StyledLink,
+} from "../styled/Navbar";
+import { Accent } from "../styled/Accent"; 
 
 export default function Navbar() {
-   return (
-      <nav>
-         <div>
-            <Link to="/">
-               Learn.Build.<span>Type.</span>
-            </Link>
-         </div>
-         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/highscores">highscores</Link></li>
-         </ul>
-      </nav>
-   )
+  return (
+    <StyledNavbar>
+      <StyledNavBrand>
+        <Link to="/">
+          Learn.Build.<Accent>Type.</Accent>
+        </Link> 
+      </StyledNavBrand>
+      <StyledNavItems>
+        <li>
+          <StyledLink to="/">Home</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/highscores">highscores</StyledLink>
+        </li>
+      </StyledNavItems>
+    </StyledNavbar>
+  );
 }
